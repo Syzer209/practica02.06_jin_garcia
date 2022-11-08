bb7ce0f7d3321be6336fe6e5e4555538af1f37bf
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-11-08 13:41:50 +0100

    Ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+direccion = input("Introduce tu dirección: ")
+telefono = int(input("Introduce tu numero de telefono: "))
+diccionario = {"su nombre es": (nombre), "tiene": (edad), "años, " "vive en": (direccion), "y su numero de telefono es": (telefono)}
+print(diccionario)
