3402f9ac481823e486948cf139354c82a64fa1d8
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-11-14 11:52:43 +0100

    Ejercicio4

---
+++ Ejercicio4.py
@@ -0,0 +1,8 @@
+diccionario = {}
+continuar = True
+while continuar:
+    clave = input("¿Que dato deseas agregar?: ")
+    valor = input(clave + " => ")
+    diccionario[clave] = valor
+    print(diccionario)
+    continuar = input("¿seguir introduciendo datos?: ") == "si"
